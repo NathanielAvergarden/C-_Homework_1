86380ebd2b76b5b88599d9aebd6b20fc9f821d63
Nathaniel <na26435@mail.ru>
2022-09-08 23:06:37 +0300

    Добавил Задачу№4

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
-﻿int a = 5;
+﻿// Задача №2
+int a = 5;
 int b = 7;
 int max = a;
 if(a > b) max = a;
 if(a < b) max = b;
+Console.WriteLine(max);
+
+// Задача №4
+int a = 2;
+int b = 3;
+int c = 7;
+int max = a;
+if(b > max) max = b;
+if(c > max) max = c;
 Console.WriteLine(max);
